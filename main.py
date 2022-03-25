c025a9e76c6e9edae5269143c21d93681802253e
esenboga <esenboga.2000@hotmail.com>
2022-03-25 14:55:33 +0300

    second push to adding new files

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-while True:
-        a = int(input("Bir sayı giriniz: "))
-        if a % 2 != 0:
-            print("Asal sayıdır.")
-        elif a==2:
-            print("Asal sayıdır.")
-        elif a % a == 0 & a % 1 == 0 :
-            print("Asal değildir.")
+print("""
+***********************************
+Dört İşlem Hesap Makinesi
+
+İşlemler:
+
+Toplama İşlemi: 1
+
+Çıkarma İşlemi: 2
+
+Çarpma İşlemi: 3
+
+Bölme İşlemi: 4
+
+
+***********************************
+""")
+islem = input("Yapmak istediğiniz işlemi giriniz: ") #girdiyi string olarak istedik
+birincisayi = float(input("İşlem yapmak istediğiniz ilk sayı giriniz: "))
+ikincisayi = float(input("İşlem yapmak istediğiniz ikinci sayı giriniz: "))
+
+
+if islem == "1": #girdiyi string olarak istediğimiz için burada da string olarak tanımladık
+    print("Toplama işleminin sonucu: ", birincisayi + ikincisayi)
+elif islem == "2": #girdiyi string olarak istediğimiz için burada da string olarak tanımladık
+    print("Çıkarma işleminin sonucu: ", birincisayi - ikincisayi)
+elif islem == "3": #girdiyi string olarak istediğimiz için burada da string olarak tanımladık
+    print("Çarpma işeminin sonucu: ", birincisayi * ikincisayi)
+elif islem == "4": #girdiyi string olarak istediğimiz için burada da string olarak tanımladık
+    print("Bölme işeminin sonucu: ", birincisayi / ikincisayi)
+    print("İşlemin kalanı: ", birincisayi % ikincisayi)
+else:
+    print("İşlem seçmediniz!")
---
+++ positive&negative.py
@@ -0,0 +1,8 @@
+number = float(input("Bir sayı girin: "))
+if number >= 0:
+    if number ==0:
+        print("Sayı 0'a eşit")
+    else:
+        print("Sayı bir pozitif sayı")
+else:
+    print("Sayı bir negatif sayı")
